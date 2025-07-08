e8ad153cb4c64043282f54200f91fe2c4664886e
amousa <a_mousa@hotmail.com>
2025-07-07 23:41:53 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "fargate_batch_execution_role_arn" {
   description = "ARN of the IAM role for Fargate Batch"
   value       = aws_iam_role.batch_fargate_execution_role.arn
 }
+
+output "batch_role_arn" {
+  description = "Batch service role ARN"
+  value       = aws_iam_role.batch_service_role.arn
+}
+
+output "batch_instance_profile_name" {
+  description = "Batch instance profile name"
+  value       = "" # Removed reference to undeclared resource
+}
